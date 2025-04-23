48026151033591984cab43b67a28b1a5c496b38f
pavankumarn05 <87231013+pavankumarn05@users.noreply.github.com>
2025-04-23 10:23:48 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -6,3 +6,6 @@ function add(a, b) {
     return `Hello, ${name}`;
   }
   
+//set OPENAI_API_KEY=pksk-proj-OtQ1oeD-xlgvZeRZfvopxOHo_Ys4-0P9vWzwhJyKO1kNHZMcV7WfJBXmpDYmA0kB7dZ9-27xP_T3BlbkFJevjH8V1NG6bFOVURY3RtCeJeYwcabskUFt-9VkgkQ8PQOmW6c2Q-FGyiaS3S4i_f9eC7DiiooA 
+
+//https://github.com/pavankumarn05/nodeproj/blob/main/logic.js
